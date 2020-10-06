a1edaa450f9c074b16e14a4715db3ae53f0ec430
lmanhes <narraflow@gmail.com>
2020-10-06 18:38:22 +0200

    Fix sqlite warning

--- app/database/__init__.py
+++ app/database/__init__.py
@@ -7,7 +7,7 @@ import settings
 
 Base = declarative_base()
 
-engine = create_engine(settings.DATABASE_URL)
+engine = create_engine(settings.DATABASE_URL, connect_args={'check_same_thread': False})
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 # create a database session and close it after finishing
